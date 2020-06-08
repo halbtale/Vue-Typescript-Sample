a3ebf6839dbbd9990723ff430307069a977ab2cd
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-06-08 17:38:09 +0200

    Created counter

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <h1>Welcome to this Vue & Typescript App</h1>
+        <counter></counter>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Counter from "./components/Counter.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        Counter
+    }
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Counter: {{counter}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class App extends Vue {
+    counter = 0;
+}
+</script>
